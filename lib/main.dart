8e5b6a229f9c824441bd45011e3b9c7fbe069400
YaraZalat <yarazalat6@gmail.com>
2025-11-27 12:17:15 +0200

    geolocator update

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-void  main() {
-  runApp( MyApp());
 
+void main() {
+  runApp(MyApp());
 }
+
 class MyApp extends StatefulWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-   Position? position;
-   _determinePosition() async {
+  Position? position;
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition();
+  }
+
+  Future<void> _determinePosition() async {
     bool serviceEnabled;
     LocationPermission permission;
 
-
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-
       return Future.error('Location services are disabled.');
     }
 
@@ -29,41 +35,42 @@ class _MyAppState extends State<MyApp> {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-
         return Future.error('Location permissions are denied');
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
       return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
+          'Location permissions are permanently denied.');
     }
 
-
-    position= await Geolocator.getCurrentPosition();
+    Position pos = await Geolocator.getCurrentPosition();
+    setState(() {
+      position = pos;
+    });
   }
-
-  void initState(){
-    super.initState();
-    _determinePosition();
-  }
-
- late final Set<Marker> mark={Marker(markerId: MarkerId("1"),position: LatLng(position!.latitude,position!.longitude))};
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(appBar: AppBar(title: Text("data"),
-      ),
-        body: GoogleMap(
-
+      home: Scaffold(
+        appBar: AppBar(title: Text("Google Map")),
+        body: position == null
+            ? Center(child: CircularProgressIndicator())
+            : GoogleMap(
           mapType: MapType.normal,
-          markers:mark ,
-          initialCameraPosition: CameraPosition(target: LatLng(position!.latitude,position!.longitude),zoom: 19,),
-          onMapCreated: (GoogleMapController controller) {
-           // _controller.complete(controller);
+          markers: {
+            Marker(
+              markerId: MarkerId("1"),
+              position:
+              LatLng(position!.latitude, position!.longitude),
+            ),
           },
+          initialCameraPosition: CameraPosition(
+            target: LatLng(position!.latitude, position!.longitude),
+            zoom: 18,
+          ),
         ),
       ),
     );
