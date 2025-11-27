751dba8111213671a9ee997b6023f2a0ff9f61d3
hesham <metwally820@gmail.com>
2025-11-27 10:50:18 +0200

    google map widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:background_locator_2/background_locator.dart';
+import 'package:background_locator_2/auto_stop_handler.dart';
+import 'package:background_locator_2/callback_dispatcher.dart';
+import 'package:background_locator_2/keys.dart';
+import 'package:background_locator_2/location_dto.dart';
+import 'package:background_locator_2/settings/android_settings.dart';
+import 'package:background_locator_2/settings/ios_settings.dart';
+import 'package:background_locator_2/settings/locator_settings.dart';
+import 'package:background_locator_2/utils/settings_util.dart';
 void main() {
   runApp( MyApp());
 }
@@ -17,7 +26,7 @@ final Set<Marker> mark={Marker(markerId: MarkerId("1"),position: LatLng(30.47386
 
           mapType: MapType.normal,
           markers:mark ,
-          initialCameraPosition: CameraPosition(target: LatLng(30.4738636,31.199931),zoom: 20,),
+          initialCameraPosition: CameraPosition(target: LatLng(30.4738636,31.199931),zoom: 19,),
           onMapCreated: (GoogleMapController controller) {
            // _controller.complete(controller);
           },
