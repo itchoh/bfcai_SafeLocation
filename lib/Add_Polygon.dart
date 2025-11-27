625ddefc5fc8867655f2147d14264d5412c9b3b9
hesham <metwally820@gmail.com>
2025-11-27 21:59:54 +0200

    Add new Polygon Page

---
+++ lib/Add_Polygon.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class AddPolygon extends StatefulWidget {
+  AddPolygon({super.key, required this.position, required this.mapController});
+  final Position? position;
+  GoogleMapController? mapController;
+
+  @override
+  State<AddPolygon> createState() => _AddPolygonState();
+}
+
+class _AddPolygonState extends State<AddPolygon> {
+
+  List<LatLng>newlistlatlng=[];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Google Map Live Tracking")),
+      body: widget.position == null
+          ? const Center(child: CircularProgressIndicator())
+          : GoogleMap(
+        onTap: (lat){
+          setState(() {
+            newlistlatlng.add(lat);
+          });
+        },
+              circles: {
+                Circle(
+                  circleId: CircleId("2"),
+                  center: LatLng(30.174999211815003, 31.203514679362613),
+                  radius: 50,
+                  fillColor: Colors.greenAccent.withOpacity(0.4),
+                  strokeWidth: 0,
+                ),
+              },
+              polygons: {
+                Polygon(
+                  polygonId: PolygonId("1"),
+                  points:newlistlatlng,
+                  fillColor: Colors.greenAccent.withOpacity(0.4),
+                  strokeWidth: 0,
+                ),
+              },
+              onMapCreated: (controller) {
+                widget.mapController = controller;
+              },
+              mapType: MapType.normal,
+              markers: {
+                Marker(
+                  markerId: const MarkerId("me"),
+                  position: LatLng(
+                    widget.position!.latitude,
+                    widget.position!.longitude,
+                  ),
+                  infoWindow: const InfoWindow(title: "You are here"),
+                ),
+              },
+              initialCameraPosition: CameraPosition(
+                target: LatLng(
+                  widget.position!.latitude,
+                  widget.position!.longitude,
+                ),
+                zoom: 17,
+              ),
+            ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import 'Add_Polygon.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -86,16 +88,82 @@ class _MyAppState extends State<MyApp> {
                     child: SizedBox(
                       height: double.infinity,
                       width: double.infinity,
+                      child: GoogleMap(
+                        circles: {
+                          Circle(
+                            circleId: CircleId("2"),
+                            center: LatLng(
+                              30.174999211815003,
+                              31.203514679362613,
+                            ),
+                            radius: 50,
+                            fillColor: Colors.greenAccent.withOpacity(0.4),
+                            strokeWidth: 0,
+                          ),
+                        },
+                        polygons: {
+                          Polygon(
+                            polygonId: PolygonId("1"),
+                            points: [
+                              LatLng(30.176321976537384, 31.200773727805625),
+                              LatLng(30.176625612941034, 31.205861636837874),
+                              LatLng(30.173874669554504, 31.20598598918641),
+                              LatLng(30.17397831334172, 31.201020714858675),
+                            ],
+                            fillColor: Colors.greenAccent.withOpacity(0.4),
+                            strokeWidth: 0,
+                          ),
+                        },
+                        onMapCreated: (controller) {
+                          mapController = controller;
+                        },
+                        mapType: MapType.normal,
+                        markers: {
+                          Marker(
+                            markerId: const MarkerId("me"),
+                            position: LatLng(
+                              position!.latitude,
+                              position!.longitude,
+                            ),
+                            infoWindow: const InfoWindow(title: "You are here"),
+                          ),
+                        },
+                        initialCameraPosition: CameraPosition(
+                          target: LatLng(
+                            position!.latitude,
+                            position!.longitude,
+                          ),
+                          zoom: 17,
+                        ),
+                      ),
                     ),
                   ),
                   Align(
                     alignment: Alignment.bottomLeft,
-                    child: ElevatedButton(onPressed: (){}, child: Text("polygon"),),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) {
+                              return AddPolygon(
+                                position: position,
+                                mapController: mapController,
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      child: Text("polygon"),
+                    ),
                   ),
                   Align(
                     alignment: Alignment.bottomRight,
-                    child: ElevatedButton(onPressed: (){}, child: Text("Circle"),),
-                  )
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Circle"),
+                    ),
+                  ),
                 ],
               ),
       ),
